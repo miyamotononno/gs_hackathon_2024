318052a30d3d90c384d7ebbad5b238030b06d1a2
Teki <w2495969292@gmail.com>
2024-05-15 00:21:41 +0900

    feat: add api endpoint to retrieve jira ticket and github pr info

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 import uvicorn
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+from github import Github
+import Constants
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
@@ -11,4 +15,57 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
+
     return {"item_id": item_id, "q": q}
+
+@app.get("/jira/{issue_key}")
+def get_jira(issue_key: str):
+    url = "https://teki0928.atlassian.net/rest/api/2/issue/" + issue_key
+
+    auth = HTTPBasicAuth(Constants.JIRA_USER_NAME, Constants.JIRA_API_KEY)
+
+    headers = {
+        "Accept": "application/json"
+    }
+
+    response = requests.request(
+        "GET",
+        url,
+        headers=headers,
+        auth=auth
+    )
+    result = {}
+    res = json.loads(response.text)
+    result["name"] = res["fields"]["summary"]
+    result["description"] = res["fields"]["description"]
+    result["labels"] = res["fields"]["labels"]
+    result["proj_name"] = res["fields"]["project"]["name"]
+    result["comments"] = res["fields"]["comment"]["comments"]
+    return result
+
+@app.get("/github/{repo_owner}/{repo_name}/{pr_id}")
+def get_code_changes(repo_owner:str, repo_name:str, pr_id: int):
+    g = Github(Constants.GITHUB_TOKEN)
+    repo = g.get_repo(f"{repo_owner}/{repo_name}")
+    pr = repo.get_pull(pr_id)
+    result = {}
+    result["title"] = pr.title
+    result["number"] = pr.number
+    result["changes"] = []
+    print(f"Checking Pull Request #{pr.number} - {pr.title}")
+    files = pr.get_files()
+    for file in files:
+        file_attr = {}
+        file_attr["file_name"] = file.filename
+        patch_content = file.patch
+        patch_lines = patch_content.split('\n')
+        added_lines = [line[2:] for line in patch_lines if line.startswith('+ ') and not line.startswith('+++')]
+        deleted_lines = [line[2:] for line in patch_lines if line.startswith('- ')]
+        file_attr["added_line_num"] = len(added_lines)
+        file_attr["deleted_line_num"] = len(deleted_lines)
+        file_attr["added_lines"] = added_lines
+        file_attr["deleted_lines"] = deleted_lines
+        result["changes"].append(file_attr)
+    return result
+
+
